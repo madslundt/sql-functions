7c3d9ea04d5ba74be86ac144a25e3974d08d6d9a
Mads Engel Lundt <madslundt@live.dk>
2019-02-07 11:57:06 +0100

    Create tables.sql

---
+++ mssql/tables.sql
@@ -0,0 +1,10 @@
+DECLARE @table_name VARCHAR(100)
+DECLARE @column_name VARCHAR(100)
+
+SET @table_name = 'table'
+SET @column_name = 'column'
+
+-- Find columns that contains @column_name in table @table_name
+SELECT COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = @table_name COLUMN_NAME LIKE '%' + @column_name + '%'
