57518dc2a8eafa52754d3deff92f6d26952c8bfc
Mads Engel Lundt <madslundt@live.dk>
2019-02-17 19:39:45 +0100

    Update tables.sql

--- mssql/tables.sql
+++ mssql/tables.sql
@@ -8,3 +8,15 @@ SET @column_name = 'column'
 SELECT COLUMN_NAME
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = @table_name AND COLUMN_NAME LIKE '%' + @column_name + '%'
+
+-- Find in all objects where @column_name is used
+SELECT OBJECT_NAME(OBJECT_ID),
+definition
+FROM sys.sql_modules
+WHERE definition LIKE '%' + @column_name + '%'
+
+-- Find in stored procedures where @column_name is used
+SELECT DISTINCT OBJECT_NAME(OBJECT_ID),
+object_definition(OBJECT_ID)
+FROM sys.Procedures
+WHERE object_definition(OBJECT_ID) LIKE '%' + @column_name + '%'
